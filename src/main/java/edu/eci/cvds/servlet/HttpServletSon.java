ab7d35c612579ada22fd6964a17507d7d72ed37a
D4v0r <davorcortes.dc@gmail.com>
2020-03-01 16:56:26 -0500

    fix issues

--- src/main/java/edu/eci/cvds/servlet/HttpServletSon.java
+++ src/main/java/edu/eci/cvds/servlet/HttpServletSon.java
@@ -11,7 +11,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.MalformedURLException;
-import java.nio.charset.MalformedInputException;
 import java.util.ArrayList;
 import java.util.Optional;
 
